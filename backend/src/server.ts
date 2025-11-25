41dcbdba0e1178b25ed15773956783c392a132c4
malidu04 <malidupahasara48@gmail.com>
2025-11-25 19:00:01 +0530

    start server file creation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,29 @@
 import app from './app';
-import { connectDatabase } from './config/database'
+import { connectDatabase } from './config/database';
+import { logger } from './utils/logger';
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDatabase();
+        app.listen(PORT, ()=>{
+            logger.info(`Server running in ${process.env.NODE_ENV} node on part ${PORT}`);
+        });
+    } catch (error) {
+        logger.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+process.on('unhandledRejection', (err: Error) => {
+    logger.error('Unhandled Promise Rejection:', err);
+    process.exit(1);
+});
+
+process.on('uncaughtException', (err: Error) => {
+  logger.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+startServer();
